6f9e8506c5c1791b195bc1b76fa14b7004a96a6e
Alex-Jr <50220189+Alex-Jr@users.noreply.github.com>
2021-07-19 09:48:53 -0700

    Começando usuario

---
+++ odontopediatria/src/main/java/usuarios.java
@@ -0,0 +1,64 @@
+
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import odontopediatria.Usuario;
+
+/**
+ * Servlet implementation class usuarios
+ */
+@WebServlet("/usuarios")
+public class usuarios extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public usuarios() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String nome = request.getParameter("nome");
+		String cpf = request.getParameter("cpf");
+		String genero = request.getParameter("genero");
+		
+		Usuario usuario = new Usuario(nome, cpf, genero);
+		
+		System.out.printf("Usuario criado nome: %s; cpf: %s; genero: %s", usuario.getNome(), usuario.getCpf(), usuario.getGenero());
+		
+		doGet(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
